1d7f5ae6a99bd31db70a852d89617c11972ee742
Angélica Feitosa <angelicamarianafeitosa@gmail.com>
2025-08-09 13:53:08 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+// lista vazia com o nome ListaGenerica
+let listaGenerica = [];
+
+// lista de linguagens de programação com 5 elementos
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang'); // adicione usando push mais 3 linguagens
+console.log(linguagensDeProgramacao); // exibe no console
+
+// lista com 3 nomes
+let listaNome1 = ['Luana', 'Pamela', 'Daniela'];
+console.log(listaNome1[0]); // exibe no console o primeiro elemento de indice 0.
+
+// lista com 3 nomes 
+let listaNome2 = ['Edinaldo', 'Daniel', 'Alexandre'];
+console.log(listaNome2[1]); // exibe no console o segundo elemento de indice 1.
+
+// lista com 3 nomes
+let listaNome3 = ['Luzivânia', 'Janyelle', 'Angélica'];
+console.log(listaNome3[2]); // exibe no console o último elemento de indice 2.
